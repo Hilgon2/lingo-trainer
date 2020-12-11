ff65de10b347c91ef6a27837c9edd062c231955c
Terry <terry.zhou@student.hu.nl>
2020-12-11 18:33:27 +0100

    Add existing word test to word filter

--- src/test/java/com/lingotrainer/domain/WordFilterTest.java
+++ src/test/java/com/lingotrainer/domain/WordFilterTest.java
@@ -8,12 +8,14 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class WordFilterTest {
     private LingoWordFilter wordFilter;
+    private List<String> words = new ArrayList<>(List.of("exists"));
 
     @BeforeEach
     void setup() {
@@ -27,9 +29,17 @@ class WordFilterTest {
                 Arguments.of("groen", true),
                 Arguments.of("schoen", true),
                 Arguments.of("alsmaar", true),
+                Arguments.of("exists", false),
                 Arguments.of("artiest", true),
                 Arguments.of("hond", false)
         );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideWordsWithDifferentLengths")
+    @DisplayName("Accept words of 5 to 7 letters")
+    void test_verify_words_of_5_6_7_letters(String word, boolean shouldAccept) {
+        assertEquals(shouldAccept, this.wordFilter.verify(word, this.words));
     }
 
     static Stream<Arguments> provideWordsWithDifferentSymbols() {
@@ -49,21 +59,15 @@ class WordFilterTest {
                 Arguments.of("ARTIEST", false),
                 Arguments.of("ARTiesT", false),
                 Arguments.of("hond", false),
+                Arguments.of("exists", false),
                 Arguments.of("!!!!!", false)
         );
-    }
-
-    @ParameterizedTest
-    @MethodSource("provideWordsWithDifferentLengths")
-    @DisplayName("Accept words of 5 to 7 letters")
-    void test_verify_words_of_5_6_7_letters(String word, boolean shouldAccept) {
-        assertEquals(shouldAccept, this.wordFilter.verify(word, new ArrayList<>()));
     }
 
     @ParameterizedTest
     @MethodSource("provideWordsWithDifferentSymbols")
     @DisplayName("Accept words with lowercase")
     void test_accept_words_with_lowercase(String word, boolean shouldAccept) {
-        assertEquals(shouldAccept, this.wordFilter.verify(word, new ArrayList<>()));
+        assertEquals(shouldAccept, this.wordFilter.verify(word, this.words));
     }
 }
